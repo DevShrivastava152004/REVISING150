90a76d735b2bbeb0aa3b26e5ceb8341f2614ddb9
Dev Shrivastava <devshrivastava621@gmail.com>
2026-05-23 06:09:57 +0530

    first commit

---
+++ ContainsDuplicate.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ContainsDuplicate
+{
+    public boolean containsduplicate(int[] arr)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : arr)
+        {
+            if(set.contains(num))
+            {
+                return true;
+            }
+                set.add(num);
+            }
+            return false;
+    }
+    public static void main(String[] args) {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        int[] arr = {1,2,3,4,5,1};
+        boolean result = cd.containsduplicate(arr);
+        System.out.println(result);
+    }
+}
